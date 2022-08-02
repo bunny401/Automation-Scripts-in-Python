ef4f6be711256c0653a19609b981ccba52a759c4
Areeba Tabassum Shoaib <90552135+bunny401@users.noreply.github.com>
2022-08-02 23:40:55 +0200

    Added Filtered issues by Severity

--- sast.py
+++ sast.py
@@ -3,7 +3,7 @@ import argparse
 import code
 from lib2to3.pgen2 import token
 import os
-from re import L
+from re import L, S
 from tokenize import Token
 import urllib 
 import requests
@@ -45,7 +45,7 @@ def asoc_auth():
         sys.exit(1)
 
         
-#To get the details of a specific scan
+#(1) To get the details of a specific scan
 # def specific_scan_details(headers, SCAN_ID):
 #     print("[+] Getting the details of the scan ")
 #     r=requests.get(URL+"Scans/StaticAnalyzer/"+ SCAN_ID, headers=headers)
@@ -53,7 +53,7 @@ def asoc_auth():
 #     print (json.dumps(pretty_json, indent=2))  
     
     
-# to get all the issues beloging to the provded Fix Group ID
+# (2) To get all the issues beloging to the provded Fix Group ID
 def specific_scans_issues(headers, SCAN_ID, FIX_GROUP_ID):
     print("[+] Getting Details of the issue of the provided Fix Group ID")
     r=requests.get(URL+"FixGroups/Scan/"+SCAN_ID+"/"+FIX_GROUP_ID+"/Issues?$filter=Status ne 'Noise'", headers=headers )
@@ -79,7 +79,28 @@ def specific_scans_issues(headers, SCAN_ID, FIX_GROUP_ID):
 
 
 
-
+#(3) To get the issues by severity
+def Issues_by_Severity(headers, SCAN_ID, Severity_Provided ):
+    print("[+] Getting Details of the issue of the provided Severity")
+    r=requests.get(URL+"Issues/Scan/"+SCAN_ID+"?$filter=Severity eq"+Severity_Provided, headers=headers )
+    # pretty_json = json.loads(r.text)  
+    # print(r.text)
+    
+    myjson=r.json()
+    ourdata=[]
+    
+    csvheader = ['ID','Language','Severity','Status','Issue Type','Location']
+    for x in myjson ['Items']:
+        listing=[x['Id'],x['Language'],x['Severity'],x['Status'],x['IssueType'],x['Location']]
+        ourdata.append(listing)
+        
+        # chane 'a' to 'w' when first time creating the file
+    with open('Scans_ordered_by_Severity.csv','a',encoding='UTF8', newline='')as f:
+        writer=csv.writer(f)
+        writer.writerow(csvheader)
+        writer.writerows(ourdata)
+        
+    print('done')
 
 
 if __name__ == "__main__":
@@ -88,7 +109,19 @@ if __name__ == "__main__":
    SCAN_ID=input("Please Provide the Scan ID:")
 #    specific_scan_details(headers, SCAN_ID)
    
-   FIX_GROUP_ID=input("Please provide the Fix Group ID: ")
-   specific_scans_issues(headers, SCAN_ID, FIX_GROUP_ID)
+FIX_GROUP_ID=input("Please provide the Fix Group ID: ")
+specific_scans_issues(headers, SCAN_ID, FIX_GROUP_ID)
+   
+Severity_Provided=input("Please enter which severity issues you want(Type 'NO' to exit): ")
+   
+if Severity_Provided == 'NO' or Severity_Provided=='No' or Severity_Provided=='no' or Severity_Provided =='no' :
+       print("[+] Exiting")
+       exit()
+else:
+       Issues_by_Severity(headers, SCAN_ID, Severity_Provided)
+       
+   
+   
+   
    
    
